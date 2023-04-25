72efa659cee6f5789db6c75a41f22144cca804b7
Micael Macedo <micaelm2009@hotmail.com>
2023-04-24 23:54:41 -0300

    feat: add arc image

--- tarefa07/calculo.php
+++ tarefa07/calculo.php
@@ -3,13 +3,15 @@ $a = $_REQUEST['a'];
 $b = $_REQUEST['b']; 
 $c = $_REQUEST['c']; 
 $delta = (($b*$b) - ((4*$a) *$c));
-$x1 =  (-1 * $b) + (sqrt($delta) / (2 * $a));
-$x2 =  (-1 * $b) - (sqrt($delta) / (2 * $a));
+$x1 =  ((-1 * $b) + (sqrt($delta)) / (2 * $a));
+$x2 =  ((-1 * $b) - (sqrt($delta)) / (2 * $a));
 $yv = ((-1 * $delta) / 4 * $a);
 $xv = (-1 * $b) / (2 * $a);
-$image = imagecreate(500, 300);
-  
-// Set the background color of image
-imagecreatefrompng("./plano-cartesiano.webp")
+$img = imagecreate(500, 500);
+   $bgcolor = imagecolorallocate($img, 150, 200, 180);
+   imagearc($img, 200, 150, 150, 150, $x2, 155, 111);
+   header("Content-Type: image/png");
+   imagepng($img);
+   imagedestroy($img);
 
 ?>
