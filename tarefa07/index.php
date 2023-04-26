932fac2b5ea96eb3518278af42aabbd61310f539
Micael-Macedo <micaelm2009@hotmail.com>
2023-04-26 10:12:52 -0300

    feat: add graphic with js

--- tarefa07/index.php
+++ tarefa07/index.php
@@ -7,14 +7,72 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="calculo.php" method="post">
-        <label for="">Valor a</label>
-        <input type="text" name="a" id="a">
-        <label for="">Valor b</label>
-        <input type="text" name="b" id="b">
-        <label for="">Valor c</label>
-        <input type="text" name="c" id="c">
-        <button>Submit</button>
-    </form>
+<canvas id="arcCanvas" width="1000" height="1000"></canvas>
+  <script>
+    var a = parseInt(<?php echo json_encode($_GET['a']); ?>) 
+    var b = parseInt(<?php echo json_encode($_GET['b']); ?>);
+    var c = parseInt(<?php echo json_encode($_GET['c']); ?>);
+    console.log(a,b,c);
+    var canvas = document.getElementById("arcCanvas")
+    var ctx = canvas.getContext("2d");
+
+    // Define o intervalo de x que deseja plotar
+    var xMin = -5;
+    var xMax = 5;
+
+    // Define o incremento de x
+    var dx = 0.1;
+
+    // Define as variáveis para armazenar os pontos x e y
+    var x, y;
+
+    // Define a cor e a largura da linha do gráfico
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = 2;
+
+    criarPlanoCartesiano();
+    gerarEquacao();
+
+
+    function gerarEquacao() {
+      ctx.strokeStyle = "red";
+      ctx.beginPath();
+      for (x = xMin; x <= xMax; x += dx) {
+        y = a * x * x + b * x + c;
+        ctx.lineTo(canvas.width / 2 + x * 50, canvas.height / 2 - y * 50);
+      }
+      ctx.stroke();
+    }
+
+    function criarPlanoCartesiano() {
+      // Desenha o eixo x
+      ctx.beginPath();
+      ctx.moveTo(0, canvas.height / 2);
+      ctx.lineTo(canvas.width, canvas.height / 2);
+      let textXValue = -50
+      for (let index = 0; index <= canvas.width; index += 98) {
+          ctx.fillText(textXValue, index , canvas.height/2 - 10);
+        
+        textXValue += 10
+        
+      }
+      
+      ctx.stroke();
+
+      // Desenha o eixo y
+      ctx.beginPath();
+      ctx.moveTo(canvas.width / 2, 0);
+      ctx.lineTo(canvas.width / 2, canvas.height);
+      let textYValue = 50
+      for (let index = 0; index <= canvas.height; index += 98) {
+        if(textYValue != 0){
+          ctx.fillText(textYValue,canvas.width/2 + 10, index + 10);
+        }
+        textYValue -= 10
+        
+      }
+      ctx.stroke();
+    }
+  </script>
 </body>
 </html>
