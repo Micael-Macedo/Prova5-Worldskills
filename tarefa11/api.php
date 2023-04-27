a0c179f2a479f31e0d14b845bf159f2012a6a40f
Micael-Macedo <micaelm2009@hotmail.com>
2023-04-27 10:57:13 -0300

    feat: add push json users to array

---
+++ tarefa11/api.php
@@ -0,0 +1,17 @@
+<?php
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+        $usersData = json_decode(file_get_contents("users.json"));
+        $userData = $_POST;
+
+        if(is_null($usersData)){
+            $array = [];
+            array_push($array, $userData); 
+            file_put_contents("users.json", json_encode($array));
+        }else{
+            array_push($usersData, $userData);
+            file_put_contents("users.json", json_encode($usersData));
+        }
+
+    }else{
+        echo file_get_contents("users.json");
+    }
---
+++ tarefa11/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="http://code.jquery.com/jquery-3.6.4.min.js"></script>
+</head>
+
+<body>
+    <form action="api.php" method="post">
+        <div>
+            <label for="">Nome</label>
+            <input type="text" name="Nome">
+        </div>
+        <div>
+            <label for="">Idade</label>
+            <input type="text" name="Idade">
+        </div>
+        <div>
+            <label for="">Email</label>
+            <input type="text" name="Email">
+        </div>
+        <button>Enviar</button>
+    </form>
+    <button onclick="baixarDados()">Baixar Dados</button>
+    <script>
+        async function baixarDados(){
+            let users = await fetch("api.php").then(r => {
+                return r.json()
+            })
+            .catch(e => console.log(e))
+            console.log(users)
+        }
+    </script>
+</body>
+
+</html>
