d004e1eb5e7971d4521822d493d42a9707cbe607
Micael-Macedo <micaelm2009@hotmail.com>
2023-04-27 10:56:24 -0300

    feat: fix: convert string to object

--- tarefa08/index.php
+++ tarefa08/index.php
@@ -9,16 +9,17 @@
 <body>
         <?php
         $time = date("Y/m/d");
-        $contacts = file_get_contents('contacts.json');
-        $tempArray = (array) json_decode($contacts);
-        $currenTime = json_encode(["current-time" => $time]);
-        if(is_null($tempArray)){
-                $tempArray = $currenTime;
+        $contacts = json_decode(file_get_contents('contacts.json'));
+        $currenTime = (object) ["current-time" => $time];
+        
+        if(is_null($contacts)){
+                $array = [];
+                array_push($array, $currenTime);
+                file_put_contents('contacts.json', json_encode($array));
         }else{
-                array_push($tempArray, $currenTime);
+                array_push($contacts, $currenTime);
+                file_put_contents('contacts.json',  json_encode($contacts));
         }
-        $jsonData = json_encode($tempArray);
-        file_put_contents('contacts.json', $jsonData);
 	?>
 </body>
 </html>
